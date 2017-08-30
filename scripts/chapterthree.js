b668a352bc9fa222e888f6d11358b6c5b4fbe338
victoriaj13@gmail.com <victoriaj13@gmail.com>
2017-08-30 17:42:04 -0400

    chapter 3 exercise 2

--- scripts/chapterthree.js
+++ scripts/chapterthree.js
@@ -27,3 +27,38 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+console.log(min(5, 17));
+
+console.log("exercise: recursion");
+
+// Recursion - We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+
+ // Zero is even.
+
+ // One is odd.
+
+// For any other number N, its evenness is the same as N - 2.
+
+// Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
+
+// Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+var isEven = function(theNumber) {
+  if (theNumber < 0) {
+    return "the number is negative";
+  } else if (theNumber === 0) {
+    return "the number is even";
+  } else if (theNumber === 1) {
+    return "the number is odd";
+  } else {
+    return isEven(theNumber - 2);
+  }
+};
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+console.log(isEven(5876));
