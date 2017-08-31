c178f66bc8353eda3fbbc405ac85474ac4839f57
victoriaj13@gmail.com <victoriaj13@gmail.com>
2017-08-31 10:31:26 -0400

    chapter 3 exercise 3

--- scripts/chapterthree.js
+++ scripts/chapterthree.js
@@ -62,3 +62,45 @@ console.log(isEven(75));
 console.log(isEven(-1));
 // → ??
 console.log(isEven(5876));
+
+console.log("exercise: bean counting");
+
+// You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length with "s".length. The returned value will be a string containing only one character (for example, "b"). The first character has position zero, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+// Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+
+// Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+function countBs(string) {
+  var count = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) === "B") {
+      count += 1;
+    }
+  }
+    return count;
+};
+console.log(countBs("BBC"));
+// → 2
+console.log(countBs("The Brown Fuzzy"));
+// → 0
+console.log(countBs("happy birthday"));
+// →
+
+// *** note - `return` stops whatever function you’re in so you need to move that outside of the for loop so that the loop runs to completion (i had the return up one level so the function was only iterating through the first character of the string and returning the count based on that alone).
+
+console.log("exercise: bean counting part 2");
+
+function countChar(string, letter) {
+  var count = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) === letter) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+console.log(countChar("kakkerlak", "k"));
+// → 4
+console.log(countChar("ubbidubbieflubberCat", "b"));
